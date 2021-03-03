353232cf4319f5b6471cb91a7bfed058ab011c72
Jai Shiv <nsoni19012001@gmail.com>
2021-03-04 02:12:20 +0530

    Personal-Blog web-app Build.

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent = "Apple wants a weekend or expensive dui want to decorate. Which is always the creator nor the duration of her life. Carrots carrots just been running a lot. Product lived in this. Financing yeast rice vegetarian or clinical portal. That they are not members, nor members of the Donec ultrices tincidunt arcu. A lot of television targeted at the undergraduate nutrition. Of life, and the mountains shall be born, ultricies quis, congue in magnis dis parturient. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. The founder of basketball and football propaganda graduated drink at the arc. Performance skirt smile at any hate for hate vulputate. Running a lot of television targeted at the undergraduate nutrition.";
+const aboutContent = "Textile manufacturing refinancing is beating. Textile manufacturing dictumst the kids elit. There diameter and boat manufacturing lorem. Consectetur adipiscing elit sagittis purus each one it is. But the price they want, but the smile Vulputate soccer massage. In some salad largest ecological. Makeup is always the laughter from her, Whosoever shall not nibh sed ac hendrerit gravida. Westinghouse peanut sauce or carrots mass of temperature. For the arrows of life, so that the earth element. In basketball largest peanut running Massa developers worth it.";
+const contactContent = "Thermal deductible until the price vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Inspections Ut et drink recipes. Minneapolis developer undergraduate homework et. Laughter pull undergraduate at iaculis in the region. Nor do some shooting movies malesuada bibendum sapien arcu vitae. Recipe sometimes varied mainstream real estate. But now targeted propaganda opportunities. Sometimes put lorem ipsum carrots undergraduate tomato soup. The cushion element of the whole, they shall neither. Basketball was pregnant dark to invest clinical zero. So that the disease in the aliquam sem mauris fringilla tincidunt. Set the temperature to photography always pull for free.";
+
+const app = express();
+app.set("view engine", "ejs");
+
+const posts = [];
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.render("home", {homeContent: homeStartingContent, posts: posts});
+});
+
+app.get("/about", function(req, res) {
+    res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res) {
+    res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res) {
+    res.render("compose");
+});
+
+app.post("/compose", function(req, res) {
+    const post = {
+        title: req.body.title,
+        content: req.body.post
+    };
+    posts.push(post);
+    res.redirect("/");
+});
+
+app.get("/posts/:postName", function(req, res) {
+    const requestedTitle = _.lowerCase(req.params.postName);
+    posts.forEach(function(post) {
+        const storedTitle = _.lowerCase(post.title);
+
+        if(storedTitle === requestedTitle) {
+            // console.log("Match Found!");
+            res.render("post", {title: post.title, content: post.content})
+        } else {
+            console.log("Not a Match!");
+        }
+    });
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
